ec63977001cef027db74d073f55f18a31621f608
Yossi <yosefshalumov@gmail.com>
2024-09-10 16:22:10 +0300

    countindex

--- Terraform/Modules/VPC/main.tf
+++ Terraform/Modules/VPC/main.tf
@@ -90,5 +90,5 @@ resource "aws_route_table" "private" {
 resource "aws_route_table_association" "private_subnet_association" {
   count          = length(aws_subnet.private_subnets)
   subnet_id      = aws_subnet.private_subnets[count.index].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private[count.index].id
 }
